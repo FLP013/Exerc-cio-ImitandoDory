1ac4c8f9bcedae210129a70bfb19ebe317f1da94
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-27 21:43:50 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Imitando Dory ---");
+
+Console.WriteLine();
+
+Console.Write("Escreva uma frase: ");
+
+Console.WriteLine(
+    Console.ReadLine()!
+    .Replace("a", "aaa")
+    .Replace("e", "eee")
+    .Replace("i", "iii" )
+    .Replace("o", "ooo")
+    .Replace("u", "uuu")
+);
